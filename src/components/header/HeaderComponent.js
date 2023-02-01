ab490b1f565741760b8c25eebcdfe3a88346bfc2
Your Name <karkibishal00@gmail.com>
2023-02-01 19:14:34 +1100

    config driven ui and props

---
+++ src/components/header/HeaderComponent.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const HeaderComponent = () => {
+  return (
+    <>
+      {/* title */}
+      <div className="header-title">Pokhara Hotel</div>
+      <div className="menu-list">
+        <ul>
+          <ol>Home</ol>
+          <ol>About</ol>
+          <ol>Contact</ol>
+          <ol>About</ol>
+        </ul>
+      </div>
+    </>
+  );
+};
